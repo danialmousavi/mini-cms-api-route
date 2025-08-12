ab52217f150abb3317f79a079f8496a5d9918df0
danialmousavi <danialmoosavi69@gmail.com>
2025-08-12 19:30:54 +0330

    handle create user api

---
+++ pages/api/courses/index.js
@@ -0,0 +1,23 @@
+import courseModel from "@/models/course";
+import connectDB from "@/utils/db";
+
+const handler = async (req, res) => {
+  connectDB();
+  if (req.method == "POST") {
+    try {
+      const { title } = req.body;
+      if (title.trim() == "" || title.trim().length < 3) {
+        return res.status(402).json({ message: "invalid value" });
+      }
+      const data = await courseModel.create({ title });
+      if (data) {
+        return res.status(200).json({ message: "user created succesfully" });
+      } else {
+        return res.status(409).json({ message: "unknown error" });
+      }
+    } catch (error) {
+        return res.status(500).json({message:"server Error !"})
+    }
+  }
+};
+export default handler;
